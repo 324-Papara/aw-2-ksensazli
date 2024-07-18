21be2e2e7eac15d039cd970d703f67fb55527d8b
Kağan <kagansensazli@gmail.com>
2024-07-18 20:08:26 +0300

    add FluentValidation Package

--- Para.Api/Para.Data/obj/Debug/net8.0/Para.Data.AssemblyInfo.cs
+++ Para.Api/Para.Data/obj/Debug/net8.0/Para.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Para.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3782a9388fbae41e7befad52c1f5595ec4615678")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a82dcbc5754b2cba743275270edc8cc4325f0d06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Para.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Para.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
